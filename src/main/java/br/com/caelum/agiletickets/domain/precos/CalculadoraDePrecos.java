470883533c3d108f26b66474969b64c11c890d9c
Alvaro Salomao Rumbane <asrumbane@gmail.com>
2016-08-24 09:33:06 +0200

    Refactoracao do codigo da classe Calculadora de precos

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -9,33 +9,40 @@ public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco;
+		int lugares = (sessao.getTotalIngressos() - sessao.getIngressosReservados());
+		double totalIngressos = 	sessao.getTotalIngressos().doubleValue();
+		BigDecimal dezPorCento = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+		BigDecimal vintePorCento = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
+		boolean cinquentaPorCento = (sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50;
 		
 		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
 			//quando estiver acabando os ingressos... 
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.05) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+			if(lugares/totalIngressos  <= 0.05) { 
+				preco = dezPorCento;
+			} else {
+							preco = sessao.getPreco();
+			}
+		} 
+		
+		else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) {
+			if(cinquentaPorCento) { 
+				preco = vintePorCento;
 			} else {
 				preco = sessao.getPreco();
 			}
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-			} else {
-				preco = sessao.getPreco();
-			}
-			
 			if(sessao.getDuracaoEmMinutos() > 60){
 				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			}
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
+		} 
+		
+		else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
 			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
+				preco = vintePorCento;
 			} else {
 				preco = sessao.getPreco();
 			}
-
 			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+				preco = dezPorCento;
 			}
 		}  else {
 			//nao aplica aumento para teatro (quem vai é pobretão)
@@ -44,5 +51,5 @@ public class CalculadoraDePrecos {
 
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
-
 }
+
